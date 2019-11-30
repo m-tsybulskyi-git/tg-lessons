c6e28b9f152966f6dd30ae4e04ad2a07ae8a1592
Nikita <43611787+GitHub44days@users.noreply.github.com>
2019-12-01 01:32:48 +0200

    lesson-1.1

---
+++ src/main/java/Main.java
@@ -0,0 +1,36 @@
+import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.TelegramBotsApi;
+import org.telegram.telegrambots.api.objects.Message;
+import org.telegram.telegrambots.api.objects.Update;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.exceptions.TelegramApiRequestException;
+
+public class Main extends TelegramLongPollingBot {
+
+    public static void main(String[] args) {
+
+        ApiContextInitializer.init();
+
+        try {
+            TelegramBotsApi telegramBotsApi = new TelegramBotsApi();
+            telegramBotsApi.registerBot(new Main());
+        } catch (TelegramApiRequestException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String getBotUsername() {
+        return "tgApiLessons_by44days_bot";
+    }
+
+    @Override
+    public String getBotToken() {
+        return "935812180:AAFfgteJ4tO6dT0xw4Yi3tcfvM3RqrX_2j0";
+    }
+
+    public void onUpdateReceived(Update update) {
+        if (update.hasMessage()) {
+            Message message = update.getMessage();
+        }
+    }
+}
