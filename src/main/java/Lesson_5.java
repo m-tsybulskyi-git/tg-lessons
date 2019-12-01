83d9ff5dd2b162cc4763e8d97d765a4683c9aa0c
Nikita <43611787+GitHub44days@users.noreply.github.com>
2019-12-01 20:03:00 +0200

    Update Lesson_5.java

--- src/main/java/Lesson_5.java
+++ src/main/java/Lesson_5.java
@@ -29,12 +29,12 @@ public class Lesson_5 extends TelegramLongPollingBot {
     }
 
     public String getBotUsername() {
-        return "tgApiLessons_by44days_bot";
+        return [BOT_USERNAME];
     }
 
     @Override
     public String getBotToken() {
-        return "935812180:AAFfgteJ4tO6dT0xw4Yi3tcfvM3RqrX_2j0";
+        return  [YOUR_SECRET_TOKEN];
     }
 
     public void onUpdateReceived(Update update) {
